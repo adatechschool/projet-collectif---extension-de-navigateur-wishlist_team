2d2a7773a5955d913bb1e7d356faf8910af6a3f2
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-05-04 12:23:32 +0200

    echec test

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 // GET URL
 
 function getWebsite() {
-let site
-    document.getElementById('save').addEventListener('click',()=>{
-        // get website
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        site = tabs[0].url
-        console.log("site1 ", site)
-        document.getElementById('url').innerText = site
-      
-     
-        // afficher comment et website pour après les afficher sur ma_liste
-        const comment = document.getElementById('comment').value
-        console.log(comment)
-        console.log(site)
-      })
-    })
-return site
+  let site;
+  document.getElementById("save").addEventListener("click", () => {
+    // get website
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      site = tabs[0].url;
+      console.log("site1 ", site);
+      document.getElementById("url").innerText = site;
+
+      // afficher comment et website pour après les afficher sur ma_liste
+      const comment = document.getElementById("comment").value;
+      console.log(comment);
+      console.log(site);
+    });
+  });
+  return site;
 }
-getWebsite()
-
-
-
-
-
-
+getWebsite();
